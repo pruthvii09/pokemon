86dac10d528f0138238fe5dd34320d34f803d696
pruthvii09 <contact.pruthvirajauti@gmail.com>
2023-12-24 01:04:09 +0530

    chnages for lazy loading

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,10 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { PokemonContext } from "../context/PokemonContext";
 import { CardPokemon } from "./CardPokemon";
 import { Loader } from "./Loader";
 
 export const PokemonList = () => {
-  const { allPokemons, loading, filteredPokemons } = useContext(PokemonContext);
+  const { loading, filteredPokemons } = useContext(PokemonContext);
+  const [offset, setOffset] = useState(0);
+  const [allPokemons, setAllPokemons] = useState([]);
+  const getAllPokemons = async (limit = 25) => {
+    const baseURL = "https://pokeapi.co/api/v2/";
+    const res = await fetch(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await res.json();
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+    console.log(allPokemons);
+    setAllPokemons([...allPokemons, ...results]);
+    console.log(allPokemons);
+
+    // setLoading(false);
+  };
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.documentElement.scrollHeight
+    ) {
+      setOffset((prev) => prev + 50);
+    }
+  };
+  useEffect(() => {
+    getAllPokemons();
+  }, [offset]);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="min-h-screen">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FilterBar, PokemonList } from "../components";
 import { PokemonContext } from "../context/PokemonContext";
 import Dropdown from "../components/Dropdown";
 
 export const HomePage = () => {
-  const { onClickLoadMore, active, setActive } = useContext(PokemonContext);
-  console.log(active);
   return (
     <div className="dark:bg-[#111827]">
       <div className="flex  sm:justify-end max-w-[1200px] mx-auto justify-center">
@@ -13,13 +11,8 @@ export const HomePage = () => {
       </div>
       <PokemonList />
       <FilterBar />
-      <div className="flex justify-center my-5  max-w-[1200px] mx-auto">
-        <button
-          className="border-none py-4 px-8 bg-emerald-500 text-white cursor-pointer rounded-xl"
-          onClick={onClickLoadMore}
-        >
-          Load More
-        </button>
+      <div className="flex justify-center my-5 text-white  max-w-[1200px] mx-auto">
+        Loading...
       </div>
     </div>
   );
